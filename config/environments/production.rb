d883710b01b6795bf102eb152d477e369f3cecf6
AAANNNAAALLLYYYSSSTTT <AAANNNAAALLLYYYSSSTTT@GOOGLEMAIL.COM>
2013-06-30 13:46:18 +0600

    Fallback to assets pipeline if a precompiled asset is missed for the production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Er::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false # This is a option from production
 
   # Generate digests for assets URLs.
   config.assets.digest = true
