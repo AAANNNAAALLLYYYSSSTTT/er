23457f933c370b860443f5dc0f666a651b5c2a83
AAANNNAAALLLYYYSSSTTT <AAANNNAAALLLYYYSSSTTT@GOOGLEMAIL.COM>
2013-06-30 13:41:06 +0600

    Init the ER project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Er::Application.config.secret_key_base = '1637cbf4ed1677b3d98be79d5f975718c561d06a3b5f49f92cb686253951fa4d19072e56679b95ffb394f6690092a9915c1f99f5c65781b11d0fd7ffeefee968'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Er::Application.config.session_store :cookie_store, key: '_er_session'
