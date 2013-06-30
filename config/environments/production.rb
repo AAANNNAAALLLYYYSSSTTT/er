777c2651b3feb78c1d050cdb99ff49a578c36267
AAANNNAAALLLYYYSSSTTT <AAANNNAAALLLYYYSSSTTT@GOOGLEMAIL.COM>
2013-06-30 13:44:01 +0600

    Code is reloaded between requests for the production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ Er::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  # config.cache_classes = true # This is a option from production
+  config.cache_classes = false # This is a option from development
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
