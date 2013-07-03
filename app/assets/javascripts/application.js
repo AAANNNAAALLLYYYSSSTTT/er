2447fbdf5b7587dc882480b91b8c22baeb0c4942
AAANNNAAALLLYYYSSSTTT <AAANNNAAALLLYYYSSSTTT@GOOGLEMAIL.COM>
2013-07-03 20:56:41 +0600

    Add javascript functions for dynamic loading JS and CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function loaded_js(url) {
+    var script_element_new = document.createElement("script");
+    script_element_new.type = "text/javascript";
+    script_element_new.async = true;
+    script_element_new.src = url;
+    var script_element_first = document.getElementsByTagName("script")[0];
+    script_element_first.parentNode.insertBefore(script_element_new, script_element_first);
+}
+
+function loaded_css(url) {
+    var stylesheet_element_new = document.createElement("link");
+    stylesheet_element_new.setAttribute("rel", "stylesheet");
+    stylesheet_element_new.setAttribute("type", "text/css");
+    stylesheet_element_new.setAttribute("href", url);
+    document.getElementsByTagName("head")[0].appendChild(stylesheet_element_new);
+}
